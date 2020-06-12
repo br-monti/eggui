54604caeef9c79324a6782b4d0aae3ceec9646e2
br-monti <bruno.smsouza@gmail.com>
2020-06-11 21:21:08 -0300

    Finalizando adequação geração automática do nome do produto

--- src/app/industry/products-edit/products-edit.component.ts
+++ src/app/industry/products-edit/products-edit.component.ts
@@ -4,7 +4,7 @@ import { ErrorHandlerService } from './../../core/error-handler.service';
 import { ToastyService } from 'ng2-toasty';
 import { ProductsService } from './../service/products.service';
 import { Product, EggType, Packing } from './../../core/model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { EggTypesService } from '../service/egg-types.service';
 import { PackingsService } from '../service/packings.service';
 
@@ -13,17 +13,15 @@ import { PackingsService } from '../service/packings.service';
   templateUrl: './products-edit.component.html',
   styleUrls: ['./products-edit.component.css']
 })
+
 export class ProductsEditComponent implements OnInit {
 
   product = new Product();
-  eggTypes = [];
-  packings = [];
+  eggTypes: EggType[];
+  packings: Packing[];
 
-  eggType = new EggType();
-  packing = new Packing();
-
-
-  productAux = new Product();
+  eggTypeSelected: EggType;
+  packingSelected: Packing;
 
   constructor(
     private productsService: ProductsService,
@@ -52,21 +50,19 @@ export class ProductsEditComponent implements OnInit {
   loadProduct(id: number) {
     this.productsService.findById(id)
     .then (product => {
-      this.product = product;
+      this.product = product;      
+      this.eggTypeSelected = this.product.eggType;
+      this.packingSelected = this.product.packing;
+
     })
     .catch(error => this.errorHandler.handle(error));
   }
 
   save(form: FormControl) {
 
-    this.loadEggType(this.product.eggType.id);
-    this.loadPacking(this.product.packing.id);
-
-    // console.log(this.eggType);
-    // console.log(this.packing);
-
-    //this.product.nick = `${this.eggType.type} ${this.packing.name}`;
-    //console.log(this.product.nick);
+    this.product.eggType = this.eggTypeSelected;
+    this.product.packing = this.packingSelected;
+    this.product.nick = `${this.eggTypeSelected.type} ${this.packingSelected.name}`;
 
     if (this.editing) {
       this.update(form);
@@ -81,7 +77,6 @@ export class ProductsEditComponent implements OnInit {
     this.productsService.create(this.product)
     .then(() => {
       this.toasty.success('Produto adicionado com sucesso');
-
       this.router.navigate(['/Products']);
     })
     .catch(error => this.errorHandler.handle(error));
@@ -99,32 +94,10 @@ export class ProductsEditComponent implements OnInit {
 
   new(form: FormControl) {
     form.reset();
-
     setTimeout(function() {
       this.product = new Product();
     }.bind(this), 1);
-
     this.router.navigate(['/Products/new']);
-  }
-
-  loadEggType(id: number) {
-    this.eggTypesService.findById(id)
-    .then (eggType => {
-      this.eggType = eggType;
-      console.log(this.eggType);
-      this.product.nick = `${this.eggType.type} `;
-    })
-    //.catch(error => this.errorHandler.handle(error));
-  }
-
-  loadPacking(id: number) {
-    this.packingsService.findById(id)
-    .then (packing => {
-     this.packing = packing;     
-     console.log(this.packing);
-     this.product.nick = `${this.packing.name}`;
-    })
-    //.catch(error => this.errorHandler.handle(error));
   }
 
   loadEggTypes() {
@@ -132,7 +105,8 @@ export class ProductsEditComponent implements OnInit {
       .then(eggTypes => {
         this.eggTypes = eggTypes
           .map(c => {
-            return ({ label: c.type, value: c.id });
+            return ({ label: c.type, value: {id: c.id, type: c.type, 
+              category: c.category, minWeight: c.minWeight, maxWeight: c.maxWeight}});
           });
       })
       .catch(error => this.errorHandler.handle(error));
@@ -143,10 +117,14 @@ export class ProductsEditComponent implements OnInit {
       .then(packings => {
         this.packings = packings
           .map(c => {
-            return ({ label: c.name, value: c.id });
+            return ({ label: c.name, value: {id: c.id, name: c.name, packingType: c.packingType,
+              quantityByPacking: c.quantityByPacking, packingByBox: c.packingByBox,
+               quantityByBox: c.quantityByBox }});
           });
       })
       .catch(error => this.errorHandler.handle(error));
   }
 
 }
+
+
