b6e3e846e8f2844ca1c41077eb45b261f3ab23ad
Bruno <bruno.smsouza@gmail.com>
2021-02-21 21:32:35 -0300

    Trocando classifications por eggBase.classifications

--- src/app/industry/classifications-edit/classifications-edit.component.ts
+++ src/app/industry/classifications-edit/classifications-edit.component.ts
@@ -4,9 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ErrorHandlerService } from './../../core/error-handler.service';
 import { ToastyService } from 'ng2-toasty';
 import { ClassificationsService } from './../service/classifications.service';
-import { Classification, EggBase} from './../../core/model';
+import { Classification, EggBase } from './../../core/model';
 import { Component, OnInit } from '@angular/core';
-import { EggTypesService } from '../service/egg-types.service';
 
 @Component({
   selector: 'app-classifications-edit',
@@ -16,14 +15,8 @@ import { EggTypesService } from '../service/egg-types.service';
 export class ClassificationsEditComponent implements OnInit {
 
   classification = new Classification();
-  showEggBaseForm = false;
   eggBase: EggBase;
-  classifications: Classification[];
   eggBases = [];
-  eggTypes = [];
-  products = [];
-  quantitys = [];
-  remaining: number;
 
   constructor(
     private classificationsService: ClassificationsService,
@@ -40,71 +33,54 @@ export class ClassificationsEditComponent implements OnInit {
       this.eggBase = new EggBase();
       this.loadEggBase(eggBaseId);
     }
-
-
   }
-
-//   onChange($event, ri) {
-//     //console.log($event.target.value);
-//     this.quantitys[ri] = $event.target.value;
-
-//     this.quantitys.forEach(quantity => {
-//       this.remaining = this.eggBase.quantity - quantity;
-//       console.log(this.remaining);
-//     });
-
-// }
 
   get editing() {
     return Boolean(this.eggBase.id);
   }
 
   loadEggBase(id: number) {
-
     this.eggBasesService.findById(id)
-    .then (eggBase => {
-      this.eggBase = eggBase;
-      this.classifications = this.eggBase.classifications;
-      this.eggBases.push(eggBase);
-    })
-    .catch(error => this.errorHandler.handle(error));
+      .then(eggBase => {
+        this.eggBase = eggBase;
+        this.eggBases.push(eggBase);
+      })
+      .catch(error => this.errorHandler.handle(error));
   }
 
   save(form: FormControl) {
     if (this.editing) {
       this.create(form);
     } else {
-      this.create (form);
+      this.create(form);
     }
   }
 
-  create(form: FormControl)  {
+  create(form: FormControl) {
+    this.eggBase.industryStatus = 'Classification';
+    this.eggBasesService.update(this.eggBase)
 
-     this.eggBase.classifications = this.classifications;
-     this.eggBase.industryStatus = 'Classification';
-     this.eggBasesService.update(this.eggBase)
-
-    .then(() => {
-      this.toasty.success('Classificação adicionada com sucesso');
-      this.router.navigate(['/Classifications']);
-    })
-    .catch(error => this.errorHandler.handle(error));
+      .then(() => {
+        this.toasty.success('Classificação adicionada com sucesso');
+        this.router.navigate(['/Classifications']);
+      })
+      .catch(error => this.errorHandler.handle(error));
   }
 
   update(form: FormControl) {
     this.classificationsService.update(this.classification)
-    .then(classification  => {
-      this.classification = classification;
-      this.toasty.success('Classificação alterada com sucesso"');
-      this.router.navigate(['/Classifications']);
-    })
-    .catch(error => this.errorHandler.handle(error));
+      .then(classification => {
+        this.classification = classification;
+        this.toasty.success('Classificação alterada com sucesso"');
+        this.router.navigate(['/Classifications']);
+      })
+      .catch(error => this.errorHandler.handle(error));
   }
 
   new(form: FormControl) {
     form.reset();
 
-    setTimeout(function() {
+    setTimeout(function () {
       this.classification = new Classification();
     }.bind(this), 1);
 
